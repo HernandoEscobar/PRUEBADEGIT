ef018b631d475033a9c3fe24fed57ca85d29a9bd
Hernando Escobar <hernandoe@gmail.com>
2023-01-18 18:28:21 -0300

    nuevos cambios

---
+++ JavaScript/function.js
@@ -0,0 +1,4 @@
+onclick=document.getElementById("demo").onclick = function() {
+    console.log("Estamos capturando evento click");
+    document.getElementById("demo").innerHTML = "Estamos probando evento en JS"
+}
